a28e38fa4a43363318fc5edff1581e622cdde60f
Numan Siddique <numan.siddique@enovance.com>
2014-07-02 12:55:30 +0530

    Add the cleanup script

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ stale_ports_cleanup
 ===================
 
 This script deletes the left over ports after a router has been deleted. Fix is already commited to raise an exception if the user tries to delete a router which has ports attached to it. 
+
+Instructions to run the script
+-----------------------------
+1. Source the openrc script with admin credentials
+2. Run the script --> python router_port_cleanup.py
+
---
+++ router_port_cleanup.py
@@ -0,0 +1,51 @@
+import os
+from neutronclient.v2_0.client import Client as NeutronClient
+
+
+class RouterPortCleanup():
+    def __init__(self):
+        kwargs = {}
+        kwargs['username'] = os.environ.get('OS_USERNAME')
+        kwargs['tenant_name'] = os.environ.get('OS_TENANT_NAME')
+        kwargs['auth_url'] = os.environ.get('OS_AUTH_URL')
+        #kwargs['endpoint_url'] = os.environ.get('OS_AUTH_URL')
+        kwargs['auth_strategy'] = 'keystone'
+        kwargs['password']= os.environ.get('OS_PASSWORD')
+        self.client = NeutronClient(**kwargs)
+        self.subnets = []
+        self.ports = []
+    
+    def _check_and_delete(self, port):
+        for subnet in self.subnets:
+            if port['fixed_ips'][0]['subnet_id'] == subnet['id']:
+                if port['fixed_ips'][0]['ip_address'] == subnet['gateway_ip']:
+                    # delete this port
+                    print "Found a stale port : "+ port['id']                    
+                    self.client.delete_port(port['id'])
+                    print "Deleted the port : " + port['id']
+                    print "\n\n"
+                    return
+
+    def clean_up_ports(self):
+        # get the subnets
+        self.subnets = self.client.list_subnets()['subnets']
+        print "subnets = \n"
+        print self.subnets
+        print "\n"
+        
+        self.ports = self.client.list_ports()['ports']
+              
+        for port in self.ports:
+            print "\n**************************"
+            print 'port =' + str(port)
+            print "\n"
+            if len(port['device_owner']) == 0 and len(port['device_id']) == 0:
+                self._check_and_delete(port)
+
+router_cleanup = RouterPortCleanup()
+router_cleanup.clean_up_ports()
+
+
+
+    
+
